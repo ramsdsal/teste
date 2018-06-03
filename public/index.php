764f726a0dd272acfbc139024c785450c00876c4
Ramiro <ramiro.workscreen@gmail.com>
2018-06-03 18:29:39 +0200

    hdaç

--- public/index.php
+++ public/index.php
@@ -1,13 +1,3 @@
 <?php
-
-$serverName = "145.24.222.167,8080"; 
-$connectionInfo = array( "Database"=>"project4App", "UID"=>"sa", "PWD"=>"Estrela0120");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
-
-if( $conn ) {
-     echo "Connection established.<br />";
-}else{
-     echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
-}
+print("HELLO");
 ?>
