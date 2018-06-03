2baef9e00d3d6034c8275903e52f3d47119683b6
Ramiro <ramiro.workscreen@gmail.com>
2018-06-03 18:44:35 +0200

    asda

---
+++ public/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# build.sh - PHP to static file generator
+DEST="./public"
+
+# Run PHP on gig folders and output HTML ready for static hosting
+find . -maxdepth 1 -type d \( -name '*-????' -o -name sample \) | sed 's/.\///' | \
+while read x; do \
+    mkdir -p "$DEST/$x/" && cd "$x" && php index.php > "../$DEST/$x/index.html" && cd ../ && \
+    echo "Processed $x" ; \
+done
